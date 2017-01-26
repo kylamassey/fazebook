73556ece48175e59a53abd870425e8974d9a4a23
Kyla Massey <sir.massey@gmail.com>
2017-01-26 12:35:54 -0500

    working fazbook GET POST DELETE

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -11,6 +12,7 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.use(methodOverride('_method'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../server/models/index');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users/index', { title: 'fazbook' });
+  models.User.findAll({}).then(function(users) {
+    res.render('users/index', {
+      title: 'all fazbook users',
+      users: users
+    });
+  });
+});
+
+router.get('/new', function(req, res, next) {
+    res.render('users/new', { title: 'new fazbook users'});
+});
+
+router.post('/', function(req, res, next) {
+   models.User.create({
+     email: req.body.email,
+     firstName: req.body.firstName,
+     lastName: req.body.lastName,
+     dob: req.body.dob
+   }).then(function(users) {
+      res.redirect('/users');
+   });
+});
+
+router.delete('/:id', function(req, res, next) {
+  models.User.destroy({
+    where: {id: req.params.id}
+  }).then(function(user) {
+    res.redirect('/users');
+  });
 });
 
 module.exports = router;
+
+ // user will look up all user information, findAll will find every occurence of the users in the query
+ // it will render the response as users/index template defined in our views folder
